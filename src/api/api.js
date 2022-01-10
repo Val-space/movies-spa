53f60805c9cf6ca5fa2a1eee2ecfe4ae8bdee10e
Lera Litfullina <litfullina@icloud.com>
2022-01-10 20:30:37 +0300

    uncommented post

--- src/api/api.js
+++ src/api/api.js
@@ -1,19 +1,18 @@
-// export const PostMoviesToServer = movie => {
-//   return fetch('http://localhost:8000/api/v1/movies', {
-//     method: 'POST',
-//     credentials: 'same-origin',
-//     headers: {
-//       'Content-Type': 'application/json;charset=utf-8',
 /* eslint-disable */ 
-//       'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjI0OTcyMjYwfQ.X31cryg_A126WLYT96PD-SLLFWSxb2SeoQZ4cvx3VhU',
-//     },
+export const PostMoviesToServer = (movie) => {
+  return fetch('http://localhost:8000/api/v1/movies', {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+      'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjI0OTcyMjYwfQ.X31cryg_A126WLYT96PD-SLLFWSxb2SeoQZ4cvx3VhU',
+    },
 
-//     body: JSON.stringify(movie),
-//   })
-//     .then(response => response.json());
-// };
+    body: JSON.stringify(movie),
+  })
+    .then(response => response.json());
+};
 
-/* eslint-disable */ 
 export const GetMoviesFromServer = () => {  
   return fetch('http://localhost:8000/api/v1/movies?sort=year&order=DESC&limit=10&offset=0', {
     method: 'GET',
